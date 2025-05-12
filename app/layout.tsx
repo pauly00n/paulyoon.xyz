afa982639467efea1d162cc0b8f401b97d2cc405
Paul Yoon <pauljinyoon@gmail.com>
2025-05-11 23:44:16 -0700

    oops

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import '/paulyoon.xyz/globals.css';
+import '/globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '@/components/theme-provider';
